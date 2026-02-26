0dc5f402ed30f4a579553ecdcc8a44148c324adb
abrahamsheva-web <abraham.sheva@gmail.com>
2026-02-26 22:44:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,13 @@ while True:
         print("Skipping send due to error.")
 
     time.sleep(60)
+
+def send_telegram_message(text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHANNEL_CHAT_ID,
+        "text": text
+    }
+
+    response = requests.post(url, json=payload)
+    print("Telegram response:", response.text)
